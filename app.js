3fdf42cc4fefa5e9e9c2fd38997c1fa750cb01f6
ParsaXpython <samkohan69@gmail.com>
2026-06-14 15:51:46 +0430

    add: add <<serve static files>> and ban who searching .html files

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello');
+app.use((req, res, next) => {
+    if (req.path.endsWith('.html')) {
+        return res.redirect('/');
+    } else {
+        next();
+    }
 });
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, () => {
     console.log(`Server Started on https://localhost:${PORT}`);
